c3cac417939979e2fc9893253008af577adb1d04
Ruhan Freitas <jspruhanfreitas@gmail.com>
2025-08-28 16:02:42 -0300

    creating menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import './styles/theme.css'
 import './styles/global.css'
 
-import { Heading } from './components/Heading'
 import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
 
 export const App = () => {
     return (
         <>
             <Container>
-                <Heading>Logo</Heading>
+                <Logo />
             </Container>
 
             <Container>
-                <Heading>Menu</Heading>
+                <Menu />
             </Container>
         </>
     )
