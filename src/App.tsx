44e69c3b07f950f548625d2e62b9fa36cdf95117
Ruhan Freitas <jspruhanfreitas@gmail.com>
2025-08-28 14:41:47 -0300

    heading component and typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { TimerIcon } from 'lucide-react'
 import { Heading } from './components/Heading'
 import './styles/global.css'
 
 export const App = () => {
     return (
         <div>
-            <Heading>Hello, World!</Heading>
+            <Heading>
+                Hello, World!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
+
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. In
                 harum dolores ipsum culpa totam ex ducimus perspiciatis, beatae
