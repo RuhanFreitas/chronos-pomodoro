c0c2ef93b3000474625f9c2abd1b8e05738fa581
Ruhan Freitas <jspruhanfreitas@gmail.com>
2025-08-28 15:23:12 -0300

    organizing  components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
 import './styles/theme.css'
 import './styles/global.css'
 
+import { Heading } from './components/Heading'
+import { Container } from './components/Container'
+
 export const App = () => {
     return (
         <>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Corporis minima quisquam aperiam harum autem modi
-                        quibusdam laudantium aut quam, doloribus ea odio facere
-                        atque eligendi quaerat non ratione? Itaque, modi!
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Corporis minima quisquam aperiam harum autem modi
-                        quibusdam laudantium aut quam, doloribus ea odio facere
-                        atque eligendi quaerat non ratione? Itaque, modi!
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Corporis minima quisquam aperiam harum autem modi
-                        quibusdam laudantium aut quam, doloribus ea odio facere
-                        atque eligendi quaerat non ratione? Itaque, modi!
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <Heading>Logo</Heading>
+            </Container>
+
+            <Container>
+                <Heading>Menu</Heading>
+            </Container>
         </>
     )
 }
