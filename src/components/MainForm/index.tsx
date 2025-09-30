d4f7f1f667fa5ebb47c537339c90e278ee5645d7
Ruhan Freitas <jspruhanfreitas@gmail.com>
2025-09-30 15:59:27 -0300

    add taskAction & taskReducer & reducer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,10 +7,10 @@ import type { TaskModel } from '../../models/TaskModel'
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
 import { getNextCycle } from '../../utils/getNextCycle'
 import { getNextCycleType } from '../../utils/getNextCycleType'
-import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes'
+import { TaskActionTypes } from '../../contexts/TaskContext/taskAction'
 
 export const MainForm = () => {
-    const { state, setState } = useTaskContext()
+    const { state, dispatch } = useTaskContext()
     const taskNameInput = useRef<HTMLInputElement>(null)
 
     const nextCycle = getNextCycle(state.currentCycle)
@@ -37,42 +37,12 @@ export const MainForm = () => {
             type: nextCycleType,
         }
 
-        const secondsRemaining = newTask.duration * 60
-
-        setState((prevState) => {
-            return {
-                ...prevState,
-                activeTask: newTask,
-                currentCycle: nextCycle,
-                secondsRemaining,
-                formattedSecondsRemaining:
-                    formatSecondsToMinutes(secondsRemaining),
-                tasks: [...prevState.tasks, newTask],
-            }
-        })
+        dispatch({ type: TaskActionTypes.START_TASK, payload: newTask })
     }
 
     const handleInterruptTask = () => {
-        setState((prevState) => {
-            return {
-                ...prevState,
-                activeTask: null,
-                secondsRemaining: 0,
-                formattedSecondsRemaining: '00:00',
-                tasks: prevState.tasks.map((task) => {
-                    if (
-                        prevState.activeTask &&
-                        prevState.activeTask.id === task.id
-                    ) {
-                        return {
-                            ...task,
-                            interruptedDate: Date.now(),
-                        }
-                    }
-
-                    return task
-                }),
-            }
+        dispatch({
+            type: TaskActionTypes.INTERRUPT_TASK,
         })
     }
 
--- src/utils/getNextCycle.ts
+++ src/utils/getNextCycle.ts
@@ -1,3 +1,3 @@
 export function getNextCycle(currentCycle: number) {
-    return currentCycle === 0 ? 1 : currentCycle + 1
+    return currentCycle === 0 || 8 ? 1 : currentCycle + 1
 }
