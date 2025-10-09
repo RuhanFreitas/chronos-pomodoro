bf5a709603d7db7b6402f577efb4b569823e7853
Ruhan Freitas <jspruhanfreitas@gmail.com>
2025-10-09 14:40:54 -0300

    add scrollToTop on about pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { Home } from './pages/Home'
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
 
 import './styles/theme.css'
 import './styles/global.css'
 import { MessagesContainer } from './components/MessagesContainer'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import { AboutPomodoro } from './pages/AboutPomodoro'
-
+import { MainRouter } from './routers/MainRouter'
 export const App = () => {
     return (
         <TaskContextProvider>
             <MessagesContainer>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/about-pomodoro"
-                            element={<AboutPomodoro />}
-                        />
-                    </Routes>
-                </BrowserRouter>
+                <MainRouter />
             </MessagesContainer>
         </TaskContextProvider>
     )
